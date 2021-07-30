a078cb4b051fd01353b98cf0b0af9b24efaf48c1
Donovan Hare <contact@donovanha.re>
2021-07-30 13:11:18 +0100

    Storybook: Slide content + footer

--- stories/Slide.stories.tsx
+++ stories/Slide.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, Story } from '@storybook/react';
 import React, { ComponentProps } from 'react';
+import { Button } from 'react-bootstrap';
 import { Navbar, Slide } from '../src';
 
 
@@ -32,3 +33,20 @@ DefaultSlide.args = {
   /*👇 The args you need here will depend on your component */
   header: (<Navbar/>)
 };
+
+
+export const WithContent = Template.bind({});
+WithContent.args = {
+  /*👇 The args you need here will depend on your component */
+  ...DefaultSlide.args,
+  children: (<h1>Content</h1>)
+};
+
+
+export const ContentAndFooter = Template.bind({});
+ContentAndFooter.args = {
+  /*👇 The args you need here will depend on your component */
+  ...WithContent.args,
+  footer: (<Button className="btn-3d">Next</Button>)
+};
+
