ce7d2f353f850558739d192ac1553e22dea5747c
Donovan Hare <contact@donovanha.re>
2021-08-10 11:55:59 +0100

    Export game module

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC, HTMLAttributes, ReactChild } from 'react';
 import './styles/toolkit.scss';
 
+export * from './components/core/interactive/Game';
 export { IModule, Module } from './components/core/Module';
 export { ISlide, Slide } from './components/core/Slide';
 export { Navbar } from './components/core/template/Navbar';
