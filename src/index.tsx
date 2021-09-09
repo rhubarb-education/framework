a76f3366cc616441ec8d928258e16ffcc7a5a5f2
Donovan Hare <contact@donovanha.re>
2021-09-09 14:33:16 +0100

    Remove defunct code

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,6 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
 import './styles/toolkit.scss';
 
 export * from './components/core/interactive/Game';
 export { IModule, Module } from './components/core/Module';
 export { ISlide, Slide } from './components/core/Slide';
 export { Navbar } from './components/core/template/Navbar';
-
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-    /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-    children?: ReactChild;
-}
-
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-/**
- * A custom Thing component. Neat!
- */
-export const Thing: FC<Props> = ({ children }) => {
-    return <div>{children || `the snozzberries taste like snozzberries`}</div>;
-};
