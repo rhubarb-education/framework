aab580af005d7eab8f65e940be79177bdad578ba
Donovan Hare <contact@donovanha.re>
2021-09-10 15:50:53 +0100

    Remove styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
-import './styles/toolkit.scss';
-
 export * from './components/core/interactive/Game';
 export { IModule, Module } from './components/core/Module';
 export { ISlide, Slide } from './components/core/Slide';
 export { Navbar } from './components/core/template/Navbar';
+
